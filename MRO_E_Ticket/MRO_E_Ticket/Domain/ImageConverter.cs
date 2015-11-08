8c5f340097b99e719e26db1f3ff68e7bc4384b5d
Roman Panteleev <romaxa95.95@gmail.com>
2015-11-08 12:38:47 +0300

    Add ImageConverter class

---
+++ MRO_E_Ticket/MRO_E_Ticket/Domain/ImageConverter.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRO_E_Ticket.Domain
+{
+    public class ImageConverter
+    {
+        private readonly double x1 = 0.299;
+        private readonly double x2 = 0.587;
+        private readonly double x3 = 0.114;
+
+        private readonly byte BinColorBlack = 255;
+        private readonly byte BinColorWhite = 0;
+
+        private int sizeHeigh = 0;
+        private int sizeWidth = 0;
+        private int binarizationThreshold = 120;
+
+        public int BinarizationThreshold { get { return binarizationThreshold; } set { binarizationThreshold = value; } }
+
+        #region Transfer To Grayscale
+        public Bitmap TransferToGrayscaleGetBitmap(Bitmap bitmap)
+        {
+            int[,] bufferArray = new int[bitmap.Width, bitmap.Height];
+            sizeWidth = bitmap.Width;
+            sizeHeigh = bitmap.Height;
+            for (int i = 0; i < (bitmap.Width); i++)
+            {
+                for (int j = 0; j < (bitmap.Height); j++)
+                {
+                    bufferArray[i, j] = GrayscalePixel(
+                        bitmap.GetPixel(i, j).R,
+                        bitmap.GetPixel(i, j).G,
+                        bitmap.GetPixel(i, j).B);
+                }
+            }
+            return CreateBitmap(bufferArray);
+        }
+
+        public int[,] TransferToGrayscaleGetArray(Bitmap bitmap)
+        {
+            int[,] bufferArray = new int[bitmap.Width, bitmap.Height];
+            sizeWidth = bitmap.Width;
+            sizeHeigh = bitmap.Height;
+            for (int i = 0; i < (bitmap.Width); i++)
+            {
+                for (int j = 0; j < (bitmap.Height); j++)
+                {
+                    bufferArray[i, j] = GrayscalePixel(
+                        bitmap.GetPixel(i, j).R,
+                        bitmap.GetPixel(i, j).G,
+                        bitmap.GetPixel(i, j).B);
+                }
+            }
+            return bufferArray;
+        }
+
+        private int GrayscalePixel(int R, int G, int B)
+        {
+            double Y = x1 * R + x2 * G + x3 * B;
+            return (int)Y;
+        }
+        #endregion
+
+        #region Binarization
+        public int[,] BinarizationThresholdMethodGetArray(Bitmap bitmap)
+        {
+            int[,] bufferArray = new int[bitmap.Width, bitmap.Height];
+            for (int i = 0; i < (bitmap.Width); i++)
+            {
+                for (int j = 0; j < (bitmap.Height); j++)
+                {
+                    bufferArray[i, j] = ResultPixelColorAfterBinarization(bitmap.GetPixel(i, j).R,
+                                                                          bitmap.GetPixel(i, j).G,
+                                                                          bitmap.GetPixel(i, j).B);
+                }
+            }
+            return bufferArray;
+        }
+
+        public Bitmap BinarizationThresholdMethodGetBitmap(Bitmap bitmap)
+        {
+            int[,] bufferArray = new int[bitmap.Width, bitmap.Height];
+            for (int i = 0; i < (bitmap.Width); i++)
+            {
+                for (int j = 0; j < (bitmap.Height); j++)
+                {
+                    bufferArray[i, j] = ResultPixelColorAfterBinarization(bitmap.GetPixel(i, j).R,
+                                                                          bitmap.GetPixel(i, j).G,
+                                                                          bitmap.GetPixel(i, j).B);
+                }
+            }
+            return CreateBitmap(bufferArray);
+        }
+
+        private byte ResultPixelColorAfterBinarization(int R, int G, int B)
+        {
+            byte resultColor = 0;
+            if (R <= 120 || G <= 120 || B <= 120)
+            {
+                resultColor = BinColorWhite;
+            }
+            else
+            {
+                resultColor = BinColorBlack;
+            }
+            return resultColor;
+        }
+        #endregion
+
+        public Bitmap CreateBitmap(int[,] arrayImage)
+        {
+            Bitmap halfBitmap = new Bitmap(sizeWidth, sizeHeigh);
+            int ScalePixel = 0;
+            for (int i = 0; i < (sizeWidth); i++)
+            {
+                for (int j = 0; j < (sizeHeigh); j++)
+                {
+                    ScalePixel = arrayImage[i, j];
+                    halfBitmap.SetPixel(i, j, Color.FromArgb(ScalePixel, ScalePixel, ScalePixel, ScalePixel));
+                }
+            }
+            return halfBitmap;
+        }
+
+
+    }
+}
+
---
+++ MRO_E_Ticket/MRO_E_Ticket/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MRO_E_Ticket")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MRO_E_Ticket")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("413df0f5-730c-45b1-b2bb-ca89b003a6c6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
